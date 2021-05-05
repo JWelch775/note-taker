a159fc3c3a2f651ec1c29c334ef260d055d6f59f
Justin Welch <justin.welch075@gmail.com>
2021-05-05 14:57:33 -0500

    added routes for rendering the notes.html page as well as a route for retrieving saved notes and adding a new note

--- server.js
+++ server.js
@@ -12,9 +12,25 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
   });
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+});
 
+app.get("/api/notes",(req, res) => {
+    var savedNotes = fs.readFileSync(path.join(__dirname, "./db/db.json"))
+    savedNotes = JSON.parse(savedNotes)
+    res.json(savedNotes)
+  });
 
-
+app.post("/api/notes",(req, res) => {
+    var savedNotes = JSON.parse(fs.readFileSync("./db/db.json"));
+    var noteID = (savedNotes.length).toString();
+    var newNote = req.body;
+    newNote.id = noteID;
+    savedNotes.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+  });
 
 
 
