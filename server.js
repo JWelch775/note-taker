a658d431eba2f20acf8ee5733e4d7b4959705dbf
Justin Welch <justin.welch075@gmail.com>
2021-05-05 14:43:41 -0500

    server running and homepage rendering

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require ("express");
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+  });
+
+
+
+
+
+
+
+
+
+  app.listen(PORT, () => {
+    console.log(`API server is listening on port ${PORT}!`);
+  });
